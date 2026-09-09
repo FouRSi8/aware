18cb93a18ef9689107e5d761b72adb7a682c65a4
Nitin Ajayakumar <nitin@humanityai.in>
2026-09-09 16:21:47 +0530

    Polish launcher icon safe-area rendering

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.aware.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
